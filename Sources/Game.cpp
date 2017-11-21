eb1d2c3f71399f8bac35a8e3c99811bd93e3ccf2
hw16a219 <hw16a219@oecu.jp>
2017-11-21 16:05:24 +0900

    ターゲットと砲台の位置変更

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -22,8 +22,8 @@ int     score;          //!< スコア
 void Start()
 {
     cloudPos = Vector2(-320, 100);
-    cannonPos = Vector2(-80, -150);
-    targetRect = Rect(80, -140, 40, 40);
+    cannonPos = Vector2(-200, -150);
+    targetRect = Rect(200, -140, 40, 40);
     bulletPos.x = -999;
     score = 0;
 }
